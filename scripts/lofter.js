237944420436e0b5c543818381eb4a106ef7340d
paxprot <30484122+paxprot@users.noreply.github.com>
2024-05-04 20:39:27 +0800

    Update lofter.js

--- scripts/lofter.js
+++ scripts/lofter.js
@@ -1,13 +1,15 @@
-const method = $request.method;
-const params = $request.url.split("?")[1];
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
-if (method === "GET") {
-    if (params.includes("x0&")) {
-        $done({});
-    } else {
-        const url = $request.url.split("?")[0]
-        $done({url});
+if (url.includes("/v1.1/watermarkcheck.api")) {
+    if (obj?.response.needwatermark) {
+        obj.response.needwatermark = false;
     }
-} else {
-    $done({});
+} else if (url.includes("/oldapi/post/detail.api")) {
+    if (obj?.response.posts[0]?.post?.blogInfo?.imageProtected) {
+        obj.response.posts[0].post.blogInfo.imageProtected = false;
+    }
 }
+
+$done({ body: JSON.stringify(obj) });
